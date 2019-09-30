f080b4bb059671e7318b4adbda4896d9ac16c286
Lachlan Driver <lachydriver@gmail.com>
2019-09-30 18:05:49 +1000

    updated database routes

--- server/routes/database.js
+++ server/routes/database.js
@@ -8,50 +8,72 @@ module.exports = function(db, app) {
     password = req.body.inputPassword;
 
     user = {};
-    user.valid = null
+    user.valid = null;
 
     console.log(username);
-    console.log(password)
+    console.log(password);
 
     const collection = db.collection("users");
 
     collection.findOne({ username: username }, function(err, data) {
-        if(!data){
-            return res.json(user)
-        }
+      if (!data) {
+        return res.json(user);
+      }
 
-        if(password === data.password){
-            user.valid = true;
-            user.username = username;
-            user.role = data.role;
-            res.json(user);
-        } else {
-            user.valid = false;
-            res.json(user);
-        }
+      if (password === data.password) {
+        user.valid = true;
+        user.username = username;
+        user.role = data.role;
+        res.json(user);
+      } else {
+        user.valid = false;
+        res.json(user);
+      }
     });
   });
 
-  app.post("/api/adduser", function(req, res){
-      user = {};
+  app.post("/api/adduser", function(req, res) {
+    user = {};
 
-      if (!req.body) {
-          return res.sendStatus(400);
-      };
+    if (!req.body) {
+      return res.sendStatus(400);
+    }
 
-      valid = true;
+    valid = true;
 
-      const collection = db.collection("users");
+    const collection = db.collection("users");
 
-      user.username = req.body.inputUsername;
-      user.role = req.body.inputRole;
-      user.email = req.body.inputEmail;
-      user.groups = [];
+    user.username = req.body.inputUsername;
+    user.role = req.body.inputRole;
+    user.email = req.body.inputEmail;
+    user.password = req.body.inputPassword;
+    user.groups = [{}];
 
-      collection.find({username: user.username}, function(err, data){
-              console.log(data);
-      });
+    collection.find({ username: user.username }).count((err, count) => {
+      if(count == 0){
+        collection.insertOne(user);
+        res.json(user);
+      } else {
+        res.send({error: 'user already exists'})
+      }
+    });
+  });
 
-      console.log("username not found");
+  app.post("/api/addgroup", function(req, res) {
+    group = {};
+    group.name = req.body.groupname;
+    group.channels = [];
+    group.group_assis = [];
+
+    const collection = db.collection("groups");
+
+    collection.find({name: group.name}).count((err, count) => {
+      if(count == 0){
+        collection.insertOne(group);
+        res.json(group)
+      } else {
+        res.send({error: 'group already exists'});
+      }
+    })
   })
 };
