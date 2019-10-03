9399d2ee92be8af85c88695f2772c84a15f8774b
Lachlan Driver <lachydriver@gmail.com>
2019-10-03 16:44:45 +1000

    finalised testing routes and comments

--- server/test.js
+++ server/test.js
@@ -6,13 +6,12 @@ let should = chai.should();
 chai.use(chaiHttp);
 
 describe("Server test", function() {
-
-    before(function(){
-        console.log("Before the test")
-    })
-    after(function(){
-        console.log("After the test")
-    })
+  before(function() {
+    console.log("Before the test");
+  });
+  after(function() {
+    console.log("After the test");
+  });
   describe("/api/adduser", () => {
     it("send true if successful", () => {
       chai
@@ -44,21 +43,6 @@ describe("Server test", function() {
     });
   });
 
-  describe("/api/login", () => {
-    it("send user object if successful", () => {
-      chai
-        .request("http://localhost:3000")
-        .post("/api/login")
-        .send({
-          inputUsername: "lachydriver",
-          inputPassword: "password"
-        })
-        .end((err, res) => {
-          res.body.should.be.a("object");
-        });
-    });
-  });
-
   describe("/api/getusers", () => {
     it("send user array if successful", () => {
       chai
@@ -77,6 +61,59 @@ describe("Server test", function() {
         .get("/api/getusers")
         .end((err, res) => {
           res.body.should.be.a("array");
+        });
+    });
+  });
+
+  describe("/api/getalluserdata", () => {
+    it("send users data array if successful", () => {
+      chai
+        .request("http://localhost:3000")
+        .get("/api/getalluserdata")
+        .end((err, res) => {
+          res.body.should.be.a("object");
+        });
+    });
+  });
+
+  describe("/api/getusergroups", () => {
+    it("send user's groups array if successful", () => {
+      chai
+        .request("http://localhost:3000")
+        .post("/api/getusergroups")
+        .send({
+          username: "lachydriver"
+        })
+        .end((err, res) => {
+          res.body.should.be.a("array");
+        });
+    });
+  });
+
+  describe("/api/deleteuser", () => {
+    it("send true if successful", () => {
+      chai
+        .request("http://localhost:3000")
+        .post("/api/deleteuser")
+        .send({
+          deleteUsername: "lachydriver"
+        })
+        .end((err, res) => {
+          res.body.should.be.a("boolean");
+        });
+    });
+  });
+
+  describe("/api/deletegroup", () => {
+    it("send true if successful", () => {
+      chai
+        .request("http://localhost:3000")
+        .post("/api/deletegroup")
+        .send({
+          deleteGroupName: "PUBG"
+        })
+        .end((err, res) => {
+          res.body.should.be.a("boolean");
         });
     });
   });
